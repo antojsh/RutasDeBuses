1fe9db0f96f1b194d6cfa42b5a384187a9c8d24d
Antonio <antoniojsh93@gmail.com>
2015-09-14 15:24:22 -0500

    No se duplican con JSON

--- server.js
+++ server.js
@@ -153,7 +153,7 @@ io.sockets.on('connection', function (socket) {
 	 				}
 	 		}
       var norepetidas = JSON.stringify(rutasEncontradas)
-      for(var i =0;i<norepetidas.length;i++){
+      for(var i =0;i<norepetidas.length-1;i++){
 
           for(var j=i+1;j<norepetidas.length;j++){
                  if(norepetidas[i].name ==norepetidas[j].name){

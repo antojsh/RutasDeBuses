bb481a8c11a0ff58ecbc61fd8571ea4faca7b461
antoniosh <antoniojsh93@gmail.com>
2015-09-29 04:28:04 +0200

    Prueba Material

--- public/Material/js/index.js
+++ public/Material/js/index.js
@@ -85,6 +85,12 @@ map.on('click', function (e) {
 $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
+     $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
 });
 $('#btnPartida').click(function() {
   //alert($(this).hasClass('btnParodesSelec'));z

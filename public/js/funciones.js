e58aa566e1b0ab334df7d42228fcbda6925f07a3
Antonio <antoniojsh93@gmail.com>
2015-10-15 12:18:17 -0500

    probando nuevos cambios

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -3,7 +3,9 @@ var fingerprint = new Fingerprint().get();
 socket.on('rutaEncontrada', rutaEncontrada)
 socket.on('rutaUnicaEncontrada',rutaUnicaEncontrada)
 socket.on('userProfile',function(data){
-localStorage.setItem("profile", data._id);
+  console.log(JSON.stringify(data))
+  if(data._id ===undefined) window.location ='http://busroute-pruebanodejs.rhcloud.com/';
+  localStorage.setItem("profile", data._id);
   $('#nomUsuario').html(data.name)
   $('#imgUsuario').attr("src",data.photo);
 })

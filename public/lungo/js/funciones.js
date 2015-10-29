858ca0a5e741de85ce2ac6b02e48b7c028f3696f
Antonio <antoniojsh93@gmail.com>
2015-10-29 14:48:53 -0500

    Pueba Google maps

--- public/lungo/js/funciones.js
+++ public/lungo/js/funciones.js
@@ -18,32 +18,32 @@ socket.on('rutaEncontrada', rutaEncontrada)
 
 socket.on('todaslasrutas',todaslasrutas)
 socket.on('rutaUnicaEncontrada',rutaUnicaEncontrada)
-// socket.on('userProfile',function(data){
-// //  console.log("Entrando :::"+JSON.stringify(data))
-// try{
+socket.on('userProfile',function(data){
+//  console.log("Entrando :::"+JSON.stringify(data))
+try{
 
-//   if(!data ) {
+  if(!data ) {
 
-//     if(localStorage.getItem('profile')!=null){
-//        $('#NomUsuario').html(localStorage.getItem('usuario'))
-//         $('#imgUsuario').attr("src",localStorage.getItem('foto'));
-//     }else{
-//       window.location='http://104.131.226.138:8080'
-//     }
-//   }else{
-//     localStorage.setItem("profile", data._id);
-//     localStorage.setItem("usuario", data.name);
-//     localStorage.setItem("foto", data.photo);
-//     $('#NomUsuario').html(data.name)
-//     $('#imgUsuario').attr("src",data.photo);
-//   }
+    if(localStorage.getItem('profile')!=null){
+       $('#NomUsuario').html(localStorage.getItem('usuario'))
+        $('#imgUsuario').attr("src",localStorage.getItem('foto'));
+    }else{
+      window.location='http://104.131.226.138:8080'
+    }
+  }else{
+    localStorage.setItem("profile", data._id);
+    localStorage.setItem("usuario", data.name);
+    localStorage.setItem("foto", data.photo);
+    $('#NomUsuario').html(data.name)
+    $('#imgUsuario').attr("src",data.photo);
+  }
 
-// }catch(err){
-//   console.log(err)
+}catch(err){
+  console.log(err)
 
-//    window.location ='http://104.131.226.138:8080';
-// }
-// })
+   window.location ='http://104.131.226.138:8080';
+}
+})
 
 socket.io.on('connect_error', function(err) {
   $('.noConnection').css('max-height','60px');
